aa9e34ac69df51e743c6c0d7f6297db13e481fbf
abdulrahman92c <chahdiabdulrahman@gmail.com>
2022-10-20 16:52:12 +0200

    full commit

---
+++ pages/page_html.inc.php
@@ -0,0 +1,7 @@
+<figure class="logo-page">
+          <img src="./asset/html_original_wordmark_logo_icon_146478.png" alt="html">
+         <figcaption>
+            <h3>Html</h3>
+
+         </figcaption>
+</figure>
---
+++ pages/page_java.inc.php
@@ -0,0 +1,7 @@
+<figure class="logo-page">
+          <img src="./asset/java_original_wordmark_logo_icon_146459.png" alt="html">
+         <figcaption>
+            <h3>Java</h3>
+
+         </figcaption>
+</figure>
